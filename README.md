9beab0dfb3a512a34dfdefa60cf72f798c337c51
Adam Voss <vossad01@gmail.com>
2017-04-23 16:52:30 -0500

    Reimplement server in Java using LSP4J

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# vscode-languageserver-node-example
-Sample language server implemented in Node
+# vscode-languageserver-java-example
+Sample language server implemented in Java using [LSP4J](https://github.com/eclipse/lsp4j)
--- client/src/extension.ts
+++ client/src/extension.ts
@@ -4,25 +4,62 @@
  * ------------------------------------------------------------------------------------------ */
 'use strict';
 
+import * as fs from "fs"
 import * as path from 'path';
+import * as net from 'net';
+import * as child_process from "child_process";
 
 import { workspace, Disposable, ExtensionContext } from 'vscode';
-import { LanguageClient, LanguageClientOptions, SettingMonitor, ServerOptions, TransportKind } from 'vscode-languageclient';
+import { LanguageClient, LanguageClientOptions, SettingMonitor, StreamInfo } from 'vscode-languageclient';
 
 export function activate(context: ExtensionContext) {
 
-	// The server is implemented in node
-	let serverModule = context.asAbsolutePath(path.join('server', 'server.js'));
-	// The debug options for the server
-	let debugOptions = { execArgv: ["--nolazy", "--debug=6009"] };
-	
-	// If the extension is launched in debug mode then the debug server options are used
-	// Otherwise the run options are used
-	let serverOptions: ServerOptions = {
-		run : { module: serverModule, transport: TransportKind.ipc },
-		debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions }
-	}
-	
+	function createServer(): Promise<StreamInfo> {
+		return new Promise((resolve, reject) => {
+			var server = net.createServer((socket) => {
+				console.log("Creating server");
+
+				resolve({
+					reader: socket,
+					writer: socket
+				});
+
+				socket.on('end', () => console.log("Disconnected"));
+			}).on('error', (err) => {
+				// handle errors here
+				throw err;
+			});
+
+			let javaExecutablePath = findJavaExecutable('java');
+
+			// grab a random port.
+			server.listen(() => {
+				// Start the child java process
+				let options = { cwd: workspace.rootPath };
+
+				let args = [
+					'-jar',
+					path.resolve(context.extensionPath, '..', 'server', 'build', 'libs', 'language-server-example-all.jar'),
+					server.address().port.toString()
+				]
+
+				let process = child_process.spawn(javaExecutablePath, args, options);
+
+				// Send raw output to a file
+				if (!fs.existsSync(context.storagePath))
+					fs.mkdirSync(context.storagePath);
+
+				let logFile = context.storagePath + '/vscode-languageserver-java-example.log';
+				let logStream = fs.createWriteStream(logFile, { flags: 'w' });
+
+				process.stdout.pipe(logStream);
+				process.stderr.pipe(logStream);
+
+				console.log(`Storing log in '${logFile}'`);
+			});
+		});
+	};
+
 	// Options to control the language client
 	let clientOptions: LanguageClientOptions = {
 		// Register the server for plain text documents
@@ -34,11 +71,48 @@ export function activate(context: ExtensionContext) {
 			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
 		}
 	}
-	
+
 	// Create the language client and start the client.
-	let disposable = new LanguageClient('languageServerExample', 'Language Server Example', serverOptions, clientOptions).start();
-	
+	let disposable = new LanguageClient('languageServerExample', 'Language Server Example', createServer, clientOptions).start();
+
 	// Push the disposable to the context's subscriptions so that the 
 	// client can be deactivated on extension deactivation
 	context.subscriptions.push(disposable);
+}
+
+// MIT Licensed code from: https://github.com/georgewfraser/vscode-javac
+function findJavaExecutable(binname: string) {
+	binname = correctBinname(binname);
+
+	// First search each JAVA_HOME bin folder
+	if (process.env['JAVA_HOME']) {
+		let workspaces = process.env['JAVA_HOME'].split(path.delimiter);
+		for (let i = 0; i < workspaces.length; i++) {
+			let binpath = path.join(workspaces[i], 'bin', binname);
+			if (fs.existsSync(binpath)) {
+				return binpath;
+			}
+		}
+	}
+
+	// Then search PATH parts
+	if (process.env['PATH']) {
+		let pathparts = process.env['PATH'].split(path.delimiter);
+		for (let i = 0; i < pathparts.length; i++) {
+			let binpath = path.join(pathparts[i], binname);
+			if (fs.existsSync(binpath)) {
+				return binpath;
+			}
+		}
+	}
+
+	// Else return the binary name directly (this will likely always fail downstream) 
+	return null;
+}
+
+function correctBinname(binname: string) {
+	if (process.platform === 'win32')
+		return binname + '.exe';
+	else
+		return binname;
 }
